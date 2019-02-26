d2186d7240b772c54e547d83a501cf623c632d98
Ara Yeressian <ayeressian2@gmail.com>
2019-02-27 00:31:08 +0400

    fixed genereated db schema formatting

--- src/renderer/generation/psql/fromViewToDbSchema.js
+++ src/renderer/generation/psql/fromViewToDbSchema.js
@@ -1,14 +1,14 @@
 function psql(schema) {
   let sqlSchema = '';
-  schema.tables.forEach((table) => {
+  schema.tables.forEach((table, index) => {
     let columnSql = '';
-    table.columns.forEach((column) => {
+    table.columns.forEach((column, index) => {
       if (column.fk) {
         const table = schema.tables.find((table) => table.name === column.fk.table);
         const type = table.columns.find((_column) => _column.name === column.fk.column).type;
-        columnSql += `${column.name} ${type}`;
+        columnSql += '  ' + column.name + ' ' + type;
       } else {
-        columnSql += `${column.name} ${column.type}`;
+        columnSql += '  ' + column.name + ' ' + column.type;
       }
       if (column.uq === true) {
         columnSql += ' UNIQUE';
@@ -20,28 +20,25 @@ function psql(schema) {
         columnSql += ' PRIMARY KEY';
       }
       if (column.fk) {
-        columnSql += ` REFERENCES ${column.fk.table}(${column.fk.column})`;
+        columnSql += ' REFERENCES ' + column.fk.table + '(' + column.fk.column + ')';
+      }
+      if (index < table.columns.length -1) {
+        columnSql += ',';
       }
       columnSql += '\n';
     });
-    sqlSchema += `CREATE TABLE ${table.name} (
-      ${columnSql}
-    )
-    `;
+    sqlSchema += 'CREATE TABLE ' + table.name + '(\n' + columnSql + ');\n';
+    if (index < schema.tables.length -1) {
+      sqlSchema += '\n';
+    }
   });
 
   return sqlSchema;
-}
-
-function mysql(schema) {
-  
 }
 
 export default (schema) => {
   switch (schema.dbType) {
     case 'psql':
       return psql(schema);
-    case 'mysql':
-      return mysql(schema);
   }
 };
